645665ef0b32a2afc29bbd5423f2a5879378184d
Sypchik <tastysypchik@proton.me>
2024-07-10 13:10:49 +0000

    Add bash

--- forkHW.sh
+++ forkHW.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-git clone https://github.com/Borschik27/shvirtd-example-python.git /opt
+wd=/opt/myProj
+git clone https://github.com/Borschik27/shvirtd-example-python.git $wd
+chown -R $(id -u):$(id -g) $wd
+docker compose -f $wd/docker-compose.yml up
